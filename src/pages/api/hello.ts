9657ea6401934a712897bd3d132ad2b6f91769af
João Victor Manke <64326253+jvcmanke@users.noreply.github.com>
2021-02-17 23:30:43 -0300

    Add Prettier

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,7 +1,7 @@
-import {NextApiHandler} from "next"
+import { NextApiHandler } from "next";
 
 const handler: NextApiHandler = (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
-}
+  res.status(200).json({ name: "John Doe" });
+};
 
-export default handler
+export default handler;
